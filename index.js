90f7a72d667e1a351dae059d1372c8079923c3c5
Yuriy Povalyaev <yura2883@mail.ru>
2020-02-14 16:39:16 +0300

    поддержка tree shaking в webpack

---
+++ index.js
@@ -0,0 +1,18 @@
+import {
+	addAttrs,
+	getEls, 
+	createEl,
+	createSvgEl,
+	findParentMatch
+} from './src/yp-core.js'
+
+export {addAttrs,
+	getEls, 
+	createEl,
+	createSvgEl,
+	findParentMatch
+}
+
+import ypTableData from './src/yp-table-data.js'
+
+export {ypTableData}
